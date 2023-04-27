6038f18a32c9766fb32c7507713465535b3a648c
mu5t474 <mustafaabdullayev294@gmail.com>
2023-04-27 17:39:20 +0300

    serializers and views are divided into small parts

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -4,17 +4,10 @@ from pathlib import Path
 from datetime import timedelta
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--pma)wpnd2@*(h81buh3&gx-rmgm#rkkubt^!(-5(_-910hz#5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
