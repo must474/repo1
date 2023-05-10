4f45a981c0a74e1bc58faf9d76675487b35123ab
mu5t474 <mustafaabdullayev294@gmail.com>
2023-05-10 13:51:56 +0300

    url added for getting new refresh token and access token

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('account/',include('app1.urls')),
     path('blog/',include("home.urls"))
 ]
